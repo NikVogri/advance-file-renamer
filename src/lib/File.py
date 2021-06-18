f45c865571c3562440ff4ed2c75fc01ae95c0984
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-06-19 00:02:01 +0200

    feat: added initial support for moving files to other disks

--- src/lib/File.py
+++ src/lib/File.py
@@ -2,28 +2,31 @@
 # File class wrapper
 # Author Nik Vogrinec - https://github.com/nikvogri
 # -----------------------------------------------------------
-import tkinter as tk
 import PTN
 import os
+import shutil
 from lib.Tmd import Tmd
 from lib.Cache import Cache
+from lib.FilePath import FilePath
+
+
 class File(Tmd, Cache):
+    supported_file_extensions = [".mp4", ".m4v", ".mkv", ".webm", ".mov", ".avi", ".wmv", ".mpg", ".flv", ".srt"]
+
     def __init__(self, original_file_path):
         self.original_file_path = original_file_path
-        self.parse_file()
 
-    def parse_file(self):
-        """ 
-            Breaks file path & name into pieces for future use
-        """
         path, extension = os.path.splitext(self.original_file_path)
 
         seperated_path = path.split('/')
 
         self.extension = extension
-        self.disk = seperated_path[0]
-        self.directories = seperated_path[1:-1]
+
+        self.original_path = FilePath(path)
+
         self.filename = seperated_path[-1]
+        self.original_filename = seperated_path[-1]
+
         self.file_data = self.parse_filename()
         self.content_type = "tv" if "season" in self.file_data else "movie"
 
@@ -32,10 +35,9 @@ class File(Tmd, Cache):
             Extracts data from filename
         """
         parsed_filename = PTN.parse(self.filename)
-
+        print(parsed_filename)
         for key, val in parsed_filename.items():
-
-            if (isinstance(val, int)):
+            if isinstance(val, int):
                 val = str(val)
                 parsed_filename[key] = str(val)
 
@@ -54,11 +56,19 @@ class File(Tmd, Cache):
         """ 
             Applies format to filename
         """
+        # TODO: better mapping system
+
         if self.filename == selected_format or selected_format == "":
             return
+
+        if self.extension not in self.supported_file_extensions:
+            print(self.extension)
+            return
+
         new_filename = selected_format
 
         if self.content_type == "movie":
+
             if "#title#" in selected_format:
                 new_filename = new_filename.replace(
                     "#title#", self.file_data["title"])
@@ -68,6 +78,10 @@ class File(Tmd, Cache):
                     "#year#", self.file_data["year"])
 
         if self.content_type == "tv":
+            if "#title#" in selected_format:
+                new_filename = new_filename.replace(
+                    "#title#", self.file_data["title"])
+
             if "#episodeTitle#" in selected_format:
                 if "episodeName" in self.file_data:
                     new_filename = new_filename.replace(
@@ -79,44 +93,76 @@ class File(Tmd, Cache):
                         print(cached)
                         episode_data = self.find_correct_episode(cached)
                         new_filename = new_filename.replace("#episodeTitle#", episode_data["title"])
-                    else: 
+                    else:
                         episodes = self.fetch_content_data(
-                        title=file_data["title"], season=file_data["season"])
+                            title=file_data["title"], season=file_data["season"])
                         episode_data = self.find_correct_episode(episodes)
 
                         new_filename = new_filename.replace("#episodeTitle#", episode_data["title"])
 
             if "#season#" in selected_format:
                 season = self.file_data["season"]
-                if (int(season) <= 9):
+                if int(season) <= 9:
                     season = '0' + str(season)
 
                 new_filename = new_filename.replace(
                     "#season#", season)
 
-            if ("#episode#" in selected_format):
+            if "#episode#" in selected_format:
                 episode = self.file_data["episode"]
-                if (int(episode) <= 9):
+                if int(episode) <= 9:
                     episode = '0' + str(episode)
 
                 new_filename = new_filename.replace(
                     "#episode#", episode)
 
-        self.original_filename = self.filename
         self.filename = new_filename
 
-    def rename(self):
+    def rename(self, new_path):
         """ 
             Renames filename to selected format
         """
-        original_file_full = f'{self.disk}/{"/".join(self.directories)}/{self.original_filename}{self.extension}'
-        new_file_full = f'{self.disk}/{"/".join(self.directories)}/{self.filename}{self.extension}'
+        original_file_full = f'{self.original_path.build_path()}/{self.original_filename}{self.extension}'
 
-        print("old", original_file_full)
-        print("new", new_file_full)
-        os.rename(original_file_full, new_file_full)
+        if new_path is None or new_path.build_path() == self.original_path.build_path():
+            new_full_path = f'{self.original_path.build_path()}/{self.filename}{self.extension}'
+            os.rename(original_file_full, new_full_path)
+        else:
+            new_built_path = new_path.build_path()
+
+            if "#year#" in new_built_path:
+                new_built_path = new_built_path.replace("#year#", self.file_data["year"])
+
+            if "#title#" in new_built_path:
+                new_built_path = new_built_path.replace("#title#", self.file_data["title"])
+
+            if "#episodeName#" in new_built_path:
+                new_built_path = new_built_path.replace("#episodeName#", self.file_data["episodeName"])
+
+            if "#episode#" in new_built_path:
+                new_built_path = new_built_path.replace("#episode#", self.file_data["episode"])
+
+            if "#season#" in new_built_path:
+                new_built_path = new_built_path.replace("#season#", self.file_data["season"])
+
+            if os.path.exists(new_built_path) is False:
+                os.makedirs(new_built_path, exist_ok=True)
+
+            # TODO: improve this -> placeholder
+            if "/" in self.filename:
+                self.filename = self.filename.split("/")[-1]
+
+            new_full_path = f'{new_built_path}\\{self.filename}{self.extension}'
+
+            if new_path.disk is not self.original_path.disk:
+                shutil.move(original_file_full, new_full_path)
+                # TODO: error validation (full disk, etc...)
+                # TODO: better UX when moving files
+            else:
+                os.rename(original_file_full, new_full_path)
 
     def find_correct_episode(self, episodes):
+        # TODO: this shouldn't be here - refactor in future
         for episode in episodes:
             if int(episode["number"]) is int(self.file_data["episode"]):
                 return episode
