ff3799a8795ceb08b6a4a75bd8247c380840980e
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-06-20 23:29:30 +0200

    feat: added additional config vars to configuration

--- src/lib/config.py
+++ src/lib/config.py
@@ -4,7 +4,13 @@
 # -----------------------------------------------------------
 from dotenv import load_dotenv
 import os
+import pathlib
 
 load_dotenv()
 
 tmdb_api_key = os.getenv("tmdb_api_key")
+tmdb_api_url = 'https://api.themoviedb.org/3'
+
+supported_file_extensions = [".mp4", ".m4v", ".mkv", ".webm", ".mov", ".avi", ".wmv", ".mpg", ".flv", ".srt"]
+
+cache_location = f"{pathlib.Path().absolute()}\\cache"
