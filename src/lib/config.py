14be65fd803078206fddc31613ef7294772d8362
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-06-27 17:53:13 +0200

    feat: added support for MKV files

--- src/lib/config.py
+++ src/lib/config.py
@@ -11,6 +11,6 @@ load_dotenv()
 tmdb_api_key = os.getenv("tmdb_api_key")
 tmdb_api_url = 'https://api.themoviedb.org/3'
 
-supported_file_extensions = [".mp4", ".m4v", ".mkv", ".webm", ".mov", ".avi", ".wmv", ".mpg", ".flv", ".srt"]
+supported_file_extensions = [".mp4", ".m4v", ".mkv", ".webm", ".mov", ".avi", ".wmv", ".mpg", ".flv", ".srt", ".MKV"]
 
 cache_location = f"{pathlib.Path().absolute()}\\cache"
