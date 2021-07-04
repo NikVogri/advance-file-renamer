f30fcd5a6e2ad2b59378f5d8e1764254f9aab1ff
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-07-04 16:12:45 +0200

    feat: added prefixe options to season and episode & comments

--- src/lib/File.py
+++ src/lib/File.py
@@ -60,15 +60,33 @@ class File():
         if self.filename == selected_format or selected_format == "":
             return
 
+        if re.match('^[A-Z]:', path): # check if disk is set; use default file location if not.
+            self.path = FilePath(self.map_str_to_values(path))
+
         self.filename = self.map_str_to_values(filename)
-        self.path = FilePath(self.map_str_to_values(path))
 
     def map_str_to_values(self, map_string):
+        """
+            Maps #value# to the actual values fetched from the filename or external data source. It maps only the values
+            that exist, otherwise it keeps the tag and continues.
+        """
         matches = re.finditer(r"#[A-Za-z0-9]*#", map_string)
 
         for match in matches:
             to_replace = match.group()
             key = to_replace.replace("#", "")
+
+            if key == "pEpisode" and "episode" in self.filename_data:
+                episode = self.filename_data['episode']
+                replace_text = f"{'0' if int(self.filename_data['episode']) < 10 else ''}{episode}"
+                map_string = map_string.replace(to_replace, replace_text)
+                continue
+
+            elif key == "pSeason" and "season" in self.filename_data:
+                    season = self.filename_data['season']
+                    replace_text = f"{'0' if int(self.filename_data['season']) < 10 else ''}{season}"
+                    map_string = map_string.replace(to_replace, replace_text)
+                    continue
 
             if key not in self.filename_data:
                 continue
